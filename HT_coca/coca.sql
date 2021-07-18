8f5bf9b0f66de54ce740ce95fc19c0697ee55e87
mpa19 <46200543+mpa19@users.noreply.github.com>
2021-07-19 01:35:39 +0200

    Update coca.sql

--- HT_coca/coca.sql
+++ HT_coca/coca.sql
@@ -6,6 +6,6 @@ INSERT INTO `items` (`name`,`label`,`limit`) VALUES
 	('chivatoscoca', 'Chivatos de coca', 50),
 	('hojacoca', 'Hoja de cocaina', 50),
 	('acidosulfurico', 'Acido sulfurico', 10),
-	('acidosulfurico', 'Acido Cloridico', 10),
+	('acidocloridico', 'Acido Cloridico', 10),
 	('dicromatopotasio', 'Dicromato de potasio', 10),
 	('restoscoca', 'Restos de cocaina', 50);
